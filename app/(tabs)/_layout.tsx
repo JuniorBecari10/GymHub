1506db59cd5a9de68967bceed20fd56a71b0f3eb
Antônio Carlos <antonioocarlos@proton.me>
2026-04-19 09:15:19 -0300

    feat: add icon to Home button

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,9 +1,21 @@
 import { Tabs } from "expo-router";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 export default function TabLayout() {
     return (
-        <Tabs>
-            <Tabs.Screen name="index" options={{ title: "Home" }} />
+        <Tabs
+            screenOptions={{
+                tabBarActiveTintColor: "#65645F",
+            }}
+        >
+            <Tabs.Screen name="index"
+                options={{
+                    title: "Home",
+                    tabBarIcon: ({ color, focused }) => (
+                        <Ionicons name={focused ? "home-sharp" : "home-outline"} color={color} size={24} />
+                    ),
+                }}
+            />
         </Tabs>
     );
 }
